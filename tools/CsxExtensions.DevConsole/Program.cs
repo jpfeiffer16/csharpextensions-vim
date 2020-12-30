29ea0cec67ef5e9d1346832122e790c8f395489c
jpfeiffer16 <joepfeiffer16@gmail.com>
2020-12-30 15:32:12 -0700

    Update the format a bit

---
+++ tools/CsxExtensions.DevConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using CsxExtensions;
+
+namespace CsxExtensions.DevConsole
+{
+    class Program
+    {
+        public class Test
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Test Next { get; set; }
+            public static Test New(int id, string name, Test next = null)
+            {
+                return new Test
+                {
+                    Id = id,
+                    Name = name,
+                    Next = next
+                };
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // new List<Test>
+            // {
+            //     Test.New(123, "Test", Test.New(124, "Test", Test.New(125, "Test",
+            //         Test.New(126, "Test", Test.New(127, "Test",
+            //             Test.New(128, "Test", Test.New(129, "Test",
+            //                 Test.New(130, "Test", Test.New(131, "Test")))))))))
+            // }.Dump();
+            //
+            // new List<Test>
+            // {
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            //     Test.New(123, "Test"),
+            // }.Dump();
+
+            new Dictionary<int, Test>
+            {
+                { 0, Test.New(123, "Test") },
+                { 1, Test.New(123, "Test") },
+                { 2, Test.New(123, "Test") },
+                { 3, Test.New(123, "Test") },
+                { 4, Test.New(123, "Test") },
+                { 5, Test.New(123, "Test") },
+                { 6, Test.New(123, "Test") },
+            }.Dump();
+        }
+    }
+}
--- tools/CsxExtensions/ObjectExtensions.cs
+++ tools/CsxExtensions/ObjectExtensions.cs
@@ -6,33 +6,89 @@ namespace CsxExtensions
 {
     public static class ObjectExtensions
     {
-        public static void Dump(this object source)
+        // public static void Dump(this object source)
+        // {
+        //     DumpValue<object>(source, 0);
+        // }
+
+        public static void Dump<T>(this T source)
         {
-            var type = source.GetType();
-            var properties = type.GetProperties();
-            Console.WriteLine("NAME: {0}", type.Name);
-            if (type.Name.StartsWith("Dictionary"))
+            DumpValue<T>(source, 0);
+        }
+
+
+        public static void DumpValue<T>(object source, int indentLevel)
+        {
+            // Check property type (Primitive or Complex)
+            // PRIMITIVE: 
+            // Print the primitive value
+            //
+            // COMPLEX: 
+            // Loop through properties and recursively call DumpValue() on them
+
+            // var type = source.GetType();
+            var type = typeof(T);
+            var isPrimitive = !(type.GetProperties().Count() > 0);
+            if (type.Name == "String")
+                isPrimitive = true;
+
+
+            Console.Write("(");
+            Console.Write(type.Name);
+
+            if (source is null)
             {
-                Console.WriteLine("Dictionary");
+                Console.Write(") ");
+                Console.Write("null");
+                return;
             }
-            else if (type.GetInterfaces().Any(i => i.Name == "IEnumerable"))
+
+            if (isPrimitive) Console.Write(") ");
+            else Console.WriteLine(")");
+
+
+            if (isPrimitive)
             {
-                Console.WriteLine("IEnumerable");
-            }
-            else if (properties.Count() > 0)
-            {
-                foreach (var property in properties)
-                {
-                    Console.Write(property.Name);
-                    Console.Write(" : ");
-                    Console.Write(type.GetProperty(property.Name).GetValue(source));
-                    Console.WriteLine();
-                }
+                // Type is primitive
+                Console.Write(source);
             }
             else
             {
-                Console.WriteLine(source);
+                // Type is complex
+                if (type.GetInterfaces().Any(i => i.Name.StartsWith("IEnumerable")))
+                {
+                    // type.GetGenericArguments().MakeGenericType()
+                    // var test = Activator.CreateInstance(typeof(List<>).MakeGenericType(type.GetGenericArguments()));
+                    var enumerableSource = source as IEnumerable<object>;
+                    foreach (var item in enumerableSource)
+                    {
+                        Indent(indentLevel + 1);
+                        var genericMethod = typeof(CsxExtensions.ObjectExtensions).GetMethod("DumpValue").MakeGenericMethod(type.GetGenericArguments());
+                        genericMethod.Invoke(null, new object[] { item, indentLevel + 2 });
+                        // DumpValue<T>(item, indentLevel + 1);
+                        // DumpValue.Invoke
+                    }
+                }
+                else
+                {
+                    foreach (var property in type.GetProperties())
+                    {
+                        Indent(indentLevel);
+                        var value = property.GetValue(source);
+                        Console.Write("{0} : ", property.Name);
+                        var propertyType = property.PropertyType;
+                        var genericMethod = typeof(CsxExtensions.ObjectExtensions).GetMethod("DumpValue").MakeGenericMethod(propertyType);
+                        genericMethod.Invoke(null, new object[] { value, indentLevel + 1 });
+                        // DumpValue<T>(value, indentLevel + 1);
+                        Console.WriteLine();
+                    }
+                }
             }
+        }
+
+        private static void Indent(int indentLevel)
+        {
+            Console.Write(string.Join(string.Empty, Enumerable.Range(0, indentLevel).Select(n => "  ")));
         }
     }
 }
