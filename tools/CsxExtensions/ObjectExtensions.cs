dd6ff7dbbba2bccd63dd35574d794d0f6b33d4c2
jpfeiffer16 <joepfeiffer16@gmail.com>
2020-12-30 16:31:33 -0700

    Some code cleanup

--- tools/CsxExtensions/ObjectExtensions.cs
+++ tools/CsxExtensions/ObjectExtensions.cs
@@ -6,37 +6,17 @@ namespace CsxExtensions
 {
     public static class ObjectExtensions
     {
-        // public static void Dump(this object source)
-        // {
-        //     DumpValue<object>(source, 0);
-        // }
-
-        public static void Dump<T>(this T source)
+        public static void Dump<T>(this T source, int indentLevel = 0)
         {
-            DumpValue<T>(source, 0);
-        }
-
-
-        public static void DumpValue<T>(object source, int indentLevel)
-        {
-            // Check property type (Primitive or Complex)
-            // PRIMITIVE: 
-            // Print the primitive value
-            //
-            // COMPLEX: 
-            // Loop through properties and recursively call DumpValue() on them
-
-            // var type = source.GetType();
             var type = typeof(T);
             var isPrimitive = !(type.GetProperties().Count() > 0);
             if (type.Name == "String")
                 isPrimitive = true;
 
-
             Console.Write("(");
             Console.Write(type.Name);
 
-            if (source is null)
+            if (source == null)
             {
                 Console.Write(") ");
                 Console.Write("null");
@@ -57,16 +37,12 @@ namespace CsxExtensions
                 // Type is complex
                 if (type.GetInterfaces().Any(i => i.Name.StartsWith("IEnumerable")))
                 {
-                    // type.GetGenericArguments().MakeGenericType()
-                    // var test = Activator.CreateInstance(typeof(List<>).MakeGenericType(type.GetGenericArguments()));
                     var enumerableSource = source as IEnumerable<object>;
                     foreach (var item in enumerableSource)
                     {
                         Indent(indentLevel + 1);
-                        var genericMethod = typeof(CsxExtensions.ObjectExtensions).GetMethod("DumpValue").MakeGenericMethod(type.GetGenericArguments());
-                        genericMethod.Invoke(null, new object[] { item, indentLevel + 2 });
-                        // DumpValue<T>(item, indentLevel + 1);
-                        // DumpValue.Invoke
+                        GetGenericDumpMethod(type.GetGenericArguments())
+                            .Invoke(null, new object[] { item, indentLevel + 2 });
                     }
                 }
                 else
@@ -77,14 +53,16 @@ namespace CsxExtensions
                         var value = property.GetValue(source);
                         Console.Write("{0} : ", property.Name);
                         var propertyType = property.PropertyType;
-                        var genericMethod = typeof(CsxExtensions.ObjectExtensions).GetMethod("DumpValue").MakeGenericMethod(propertyType);
-                        genericMethod.Invoke(null, new object[] { value, indentLevel + 1 });
-                        // DumpValue<T>(value, indentLevel + 1);
+                        GetGenericDumpMethod(propertyType)
+                            .Invoke(null, new object[] { value, indentLevel + 1 });
                         Console.WriteLine();
                     }
                 }
             }
         }
+
+        private static System.Reflection.MethodInfo GetGenericDumpMethod(params Type[] genericTypes) =>
+            typeof(CsxExtensions.ObjectExtensions).GetMethod("Dump").MakeGenericMethod(genericTypes);
 
         private static void Indent(int indentLevel)
         {
